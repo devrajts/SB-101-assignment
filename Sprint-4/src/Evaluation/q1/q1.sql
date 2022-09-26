dafe108d658e7e6e9fcaef865b17776b758c4614
devrajts <devrajdts11@gmail.com>
2022-09-26 12:42:33 +0530

    Update q1.sql

--- Sprint-4/src/Evaluation/q1/q1.sql
+++ Sprint-4/src/Evaluation/q1/q1.sql
@@ -122,3 +122,111 @@ mysql> select * from studentcourse;
 
 
 --------------------------------------------------------------------------------------------------------------------------------
+
+mysql> insert into studentcourse values(2,333);
+Query OK, 1 row affected (0.08 sec)
+
+mysql> select * from studentcourse;
++------+------+
+| roll | cid  |
++------+------+
+|    1 |  101 |
+|    2 |  102 |
+|    3 |  201 |
+|    1 |  101 |
+|    4 |  101 |
+|    4 |  102 |
+|    5 |  101 |
+|    5 |  201 |
+|    2 |  333 |
++------+------+
+9 rows in set (0.00 sec)
+
+---------------------------------------------------------------------------------------------------------------------------------
+
+mysql> insert into student values(6,"sursh","hubli","12356454");
+Query OK, 1 row affected (0.22 sec)
+
+mysql> insert into studentcourse values(6,101);
+Query OK, 1 row affected (0.10 sec)
+
+mysql>  insert into studentcourse values(6,103);
+Query OK, 1 row affected (0.17 sec)
+
+mysql>  insert into studentcourse values(6,201);
+Query OK, 1 row affected (0.16 sec)
+
+mysql> select * from studentcourse;
++------+------+
+| roll | cid  |
++------+------+
+|    1 |  101 |
+|    2 |  102 |
+|    3 |  201 |
+|    1 |  101 |
+|    4 |  101 |
+|    4 |  102 |
+|    5 |  101 |
+|    5 |  201 |
+|    2 |  333 |
+|    6 |  101 |
+|    6 |  103 |
+|    6 |  201 |
++------+------+
+12 rows in set (0.00 sec)
+
+-------------------------------------------------------------------------------------------------------------------------------
+
+mysql> insert into student values(7,"binay","davanagere","16546451");
+Query OK, 1 row affected (0.13 sec)
+
+mysql> insert into studentcourse values(7,201);
+Query OK, 1 row affected (0.14 sec)
+
+mysql> select * from student;
++------+--------+------------+------------+
+| roll | name   | address    | mobile     |
++------+--------+------------+------------+
+|    1 | dev    | davanagere | 8105848723 |
+|    2 | raj    | bangalore  | 654135654  |
+|    3 | kumar  | mumbai     | 468888746  |
+|    4 | amith  | chennai    | 65431351   |
+|    5 | venkat | mysore     | 6544564    |
+|    6 | sursh  | hubli      | 12356454   |
+|    7 | binay  | davanagere | 16546451   |
++------+--------+------------+------------+
+7 rows in set (0.00 sec)
+
+mysql> select * from course;j
++-----+-----------+-------+----------+
+| cid | cname     | fee   | duration |
++-----+-----------+-------+----------+
+| 101 | java      | 50000 | 6        |
+| 102 | spring    | 30000 | 5        |
+| 103 | hibernate | 70000 | 8        |
+| 201 | react     | 30000 | 5        |
+| 333 | angular   | 60000 | 7        |
++-----+-----------+-------+----------+
+5 rows in set (0.00 sec)
+
+mysql> select * from studentcourse;
++------+------+
+| roll | cid  |
++------+------+
+|    1 |  101 |
+|    2 |  102 |
+|    3 |  201 |
+|    1 |  101 |
+|    4 |  101 |
+|    4 |  102 |
+|    5 |  101 |
+|    5 |  201 |
+|    2 |  333 |
+|    6 |  101 |
+|    6 |  103 |
+|    6 |  201 |
+|    7 |  201 |
++------+------+
+13 rows in set (0.00 sec)
+
+--------------------------------------------------------------------------------------------------------------------------------
